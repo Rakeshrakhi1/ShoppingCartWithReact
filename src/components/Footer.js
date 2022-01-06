9c0bbb3cdbe7069da3d862bb197a977aac0725df
Rakeshrakhi1 <93132093+Rakeshrakhi1@users.noreply.github.com>
2022-01-06 09:54:17 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col, Navbar } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Navbar bg='dark'>
+      <Container> 
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+      </Navbar>
+    </footer>
+  )
+}
+
+export default Footer
